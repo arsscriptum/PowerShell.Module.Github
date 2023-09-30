f3c1ae968034690bedc497161ab46289519de389
arsscriptum <guillaumeplante.qc@gmail.com>
2023-09-30 00:19:09 -0400

    auto

--- tools/Start-SaveStatsJob.ps1
+++ tools/Start-SaveStatsJob.ps1
@@ -31,7 +31,7 @@ function Start-SaveStatsJob{
                     $LastUpdateTime = (Get-Date)
                     $StrDate = (Get-Date)
                     Write-Host "[Update] num $id started on $StrDate"
-                    Update-GithubSavedStats
+                    $Null = Update-GithubSavedStats
                     $StrDate = (Get-Date)
                     Write-Host "[Update] num $id ended on $StrDate"
                     $id++
